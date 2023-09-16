0a93424612e76d462a26a5b0cdc27f48eaf5f882
ShahriarAnuvab <shahriarrahmananuvab@gmail.com>
2023-09-17 03:18:25 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import profile from '../assets/profile.png'
+const Header = () => {
+    return (
+        <div className='container mx-auto flex items-center justify-between p-5 border-b-2  '>
+            <h1 className="text-[#111] text-2xl md:text-4xl font-bold">Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+            
+        </div>
+    );
+};
+
+export default Header;
